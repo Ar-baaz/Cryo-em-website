7577c6006500bcb9f526bc246d836af741f692cb
Ar-baaz <mdarbazpc@gmail.com>
2023-06-24 16:22:39 +0530

    Cryo-em website

--- script/script.js
+++ script/script.js
@@ -8,25 +8,5 @@ menuIcon.addEventListener('click', () => {
     navbg.classList.toggle('active');
 });
 
-// Function to animate the count-up effect
-function animateValue(element, start, end, duration) {
-    let current = start;
-    const range = end - start;
-    const increment = end > start ? 1 : -1;
-    const stepTime = Math.abs(Math.floor(duration / range));
-    const timer = setInterval(() => {
-      current += increment;
-      element.textContent = current;
-      if (current === end) {
-        clearInterval(timer);
-      }
-    }, stepTime);
-  }
-  
-  // Start the count-up animation for each achievement
-  const countElements = document.querySelectorAll('.count');
-  countElements.forEach((element) => {
-    const endValue = parseInt(element.textContent);
-    animateValue(element, 0, endValue, 2000);
-  });
+
   
